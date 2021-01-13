7361e5d75703570c93106403a7f3a0e70910faad
github username <scott.gloyna01@gmail.com>
2021-01-13 17:24:26 -0600

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
 # Add your code here
+require 'pry'
+class Dog
+    attr_reader :name
+
+    @@all =[]
+    @@all_names=[]
+    def initialize(name)
+        @name=name
+        self.save
+    end
+
+    def self.all
+        @@all
+    end
+    def self.clear_all
+        @@all = []
+        @@all_names =[]
+    end
+    def self.print_all
+
+        @@all_names.each {|name|puts name}
+    end
+    def save
+        @@all<<self
+        @@all_names<<@name
+    end
+end
